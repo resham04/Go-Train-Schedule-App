ff9b22a8a76c7c541b5a714b49c211f136afdbf2
Resham Mittal <35680623+resham04@users.noreply.github.com>
2023-04-14 11:07:41 -0400

    test update

---
+++ schedule.test.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const app = require("../backend/app");
+const cache = require("memory-cache");
+
+describe("GET /schedule", () => {
+  beforeEach(() => {
+    // clear cache before each test
+    cache.clear();
+  });
+
+  test("returns cached schedule if available", async () => {
+    const trainTimeTable = [{ train: "A", time: "1000" }];
+    cache.put("schedule", trainTimeTable, 60 * 1000);
+
+    const response = await request(app).get("/schedule");
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("Full Schedule fetched successfully");
+    expect(response.body.schedule).toEqual(trainTimeTable);
+  });
+
+  test("returns full schedule if not cached", async () => {
+    const response = await request(app).get("/schedule");
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("Full Schedule fetched successfully");
+    expect(response.body.schedule).toBeTruthy();
+  });
+});
