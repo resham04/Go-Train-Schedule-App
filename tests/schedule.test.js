3214132acd1ace97102291b2d8317a30c60fb4d3
Resham Mittal <35680623+resham04@users.noreply.github.com>
2023-04-14 12:13:05 -0400

    correction

--- tests/schedule.test.js
+++ tests/schedule.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("./backend/app");
+const app = require("../backend/app");
 const cache = require("memory-cache");
 
 describe("GET /schedule", () => {
